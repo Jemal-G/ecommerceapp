c3fc978c2d05568957f86452ef6f5feed8183658
Jemal-G <jgeda@madisoncollege.edu>
2025-02-22 14:08:54 -0600

    added some extra code

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import Calculator from './calculator';
-import './App.css'
+import React from "react";
+import Calculator from "./calculator";
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='container'>
-      <h2>
-        Arithmetic calculation
-      </h2>
+    <div className="container">
+      <h1>Arithmetic Calculator</h1>
+
+      {/* Pass two numbers */}
       <Calculator num1={10} num2={5} />
 
+      {/* Pass multiple numbers */}
+      <Calculator numbers={[20, 5, 2]} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- project2/src/calculator.jsx
+++ project2/src/calculator.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 
-function calculator ({num1, num2}) {
+function Calculator({ num1, num2, numbers }) {
+  if (numbers && numbers.length > 0) {
+    // Perform calculations on multiple numbers
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    const difference = numbers.reduce((acc, num) => acc - num);
+    const product = numbers.reduce((acc, num) => acc * num, 1);
+    const modulus = numbers.reduce((acc, num) => acc % num);
+    const division =
+      numbers.slice(1).includes(0) 
+        ? "Cannot divide by zero"
+        : numbers.reduce((acc, num) => acc / num);
 
     return (
-        <div className="calculation">
-            <h3>The first number is : &nbsp;{num1}</h3>
-            <h3>The second numberis :&nbsp; {num2}</h3>
-            <p>Addition of two Number:&nbsp; &nbsp;{num1 + num2}</p>
-            <p>Substraction of two Number: &nbsp;&nbsp;&nbsp;{num2 - num1}</p>
-            <p>Multiplaction of Number: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{num1 * num2}</p>
-            <p>Modulus or remainder of Number : &nbsp;&nbsp;{num1 % num2}</p>
-            <p>Division of Number not divided to O : {num2 !== 0 ?(num1/num2).toFixed(1) : "Cannot divide by zero"}</p>
-        </div>
+      <div className="calculator">
+        <h2>Numbers: {numbers.join(", ")}</h2>
+        <p>Sum: {sum}</p>
+        <p>Difference: {difference}</p>
+        <p>Product: {product}</p>
+        <p>Modulus: {modulus}</p>
+        <p>Division: {division}</p>
+      </div>
     );
+  }
+
+  // If num1 and num2 are provided, perform calculations on them
+  return (
+    <div className="calculation">
+      <h3>The first number is: {num1}</h3>
+      <h3>The second number is: {num2}</h3>
+      <p>Addition of two Numbers: {num1 + num2}</p>
+      <p>Subtraction of two Numbers: {num2 - num1}</p>
+      <p>Multiplication: {num1 * num2}</p>
+      <p>Modulus: {num1 % num2}</p>
+      <p>Division: {num2 !== 0 ? (num1 / num2).toFixed(1) : "Cannot divide by zero"}</p>
+    </div>
+  );
 }
 
-export default calculator;
+export default Calculator;
