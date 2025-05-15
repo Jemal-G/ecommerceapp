67eedff51106bf5fb68ede2860353257581d4330
Jemal-G <jgeda@madisoncollege.edu>
2025-05-15 11:30:03 -0500

    starting basic authication

--- basic-authentication/src/main.jsx
+++ basic-authentication/src/main.jsx
@@ -1,24 +1,10 @@
-
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import config from './aws-exports'
-import { Router } from 'react-router'
-import 'antd/dist/reset.css'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App';
 import './index.css'
-// main.jsx or App.jsx
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
-
-
-Amplify.configure(config)
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
--- basic-authentication/vite.config.js
+++ basic-authentication/vite.config.js
@@ -1,10 +1,7 @@
-// filepath: vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['aws-amplify'],
-  },
-});
+})
